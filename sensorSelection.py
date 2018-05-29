740cae8a2c4dd942741dd4716633d0e78f75232c
cmab92 <christopher.bonenberger@googlemail.com>
2018-05-29 17:59:04 +0200

    ....

--- sensorSelection.py
+++ sensorSelection.py
@@ -5,12 +5,12 @@ import pandas as pd
 
 ## load data
 from abraxasOne.loadAndClean import loadAndClean
-#irData, forceData, quatData, linAccData, angVecData = loadAndClean("_20185161226.txt", 10, 4, tSample = 0.0165, dirPath = "/home/bonenberger/Dokumente/Rabe/Daten/dataRABE/")
+irData, forceData, quatData, linAccData, angVecData = loadAndClean("_20185161226.txt", 10, 4, tSample = 0.0165, dirPath = "/home/bonenberger/Dokumente/Rabe/Daten/dataRABE/")
 #irData, forceData, quatData, linAccData, angVecData = loadAndClean("langsamChris_20185291452.txt", 10, 4, tSample = 0.0165, dirPath = "/home/bonenberger/Dokumente/Rabe/Daten/dataRABE/")
 #irData, forceData, quatData, linAccData, angVecData = loadAndClean("chris_20185291448.txt", 10, 4, tSample = 0.0165, dirPath = "/home/bonenberger/Dokumente/Rabe/Daten/dataRABE/")
-#irData, forceData, quatData, linAccData, angVecData = loadAndClean("sashi_20185291449.txt", 10, 4, tSample = 0.0165, dirPath = "/home/bonenberger/Dokumente/Rabe/Daten/dataRABE/")
-dir = "schnellChris_20185291452.txt"
-irData, forceData, quatData, linAccData, angVecData = loadAndClean(dir, 10, 4, tSample = 0.0165, dirPath = "/home/bonenberger/Dokumente/Rabe/Daten/dataRABE/")
+#irData, forceData, schnellChris_20185291452quatData, linAccData, angVecData = loadAndClean("sashi_20185291449.txt", 10, 4, tSample = 0.0165, dirPath = "/home/bonenberger/Dokumente/Rabe/Daten/dataRABE/")
+#dir = "toshiba2_20185291523.txt"
+#irData, forceData, quatData, linAccData, angVecData = loadAndClean(dir, 10, 2, tSample = 0.0165, dirPath = "/home/bonenberger/pyAbraxas/")
 ## denoise
 from abraxasOne.gaussFilter import gaussFilter
 
@@ -24,13 +24,15 @@ X = []
 autoCorrX = []
 corrX = []
 for i in range(len(irData)):
-    data = irData[i][1000:7000,1]
-    data = 1/np.max(data)/(1-np.min(data)/np.max(data))*data - (1/np.max(data)/(1-np.min(data)/np.max(data))*np.min(data))*np.ones([len(data)])
+    data = irData[i][::,1]
+    #plt.plot(data)
+    #plt.show()
+    #data = 1/np.max(data)/(1-np.min(data)/np.max(data))*data - (1/np.max(data)/(1-np.min(data)/np.max(data))*np.min(data))*np.ones([len(data)])
     X.append(data)
     autoCorrX.append(autoCorr(x = data, lagUpTo=200))
     corrXtemp = []
     for j in range(len(irData)):
-        corrXtemp.append(pd.Series(data).corr(other=pd.Series(irData[j][1000:7000,1])))
+        corrXtemp.append(pd.Series(data).corr(other=pd.Series(irData[j][::,1])))
     corrX.append(corrXtemp)
 corrX = np.array(corrX)
 print(corrX)
