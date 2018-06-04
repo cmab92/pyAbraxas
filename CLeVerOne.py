01de7437b4adcbb899f4f424d7d666b1fd22c8b0
cmab92 <christopher.bonenberger@googlemail.com>
2018-06-04 18:22:11 +0200

    ..............UNSINN

--- CLeVerOne.py
+++ CLeVerOne.py
@@ -4,6 +4,8 @@
 ## general info:
 ## algorithm from "Feature Subset Selection And Feature Ranking For Multivariate Time Series", Yoon et al. , 2005
 ##
+## denoising by gauss???!!!
+##
 ## function inputs:
 ##
 ## functions output:
@@ -13,20 +15,33 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
+from abraxasOne.gaussFilter import gaussFilter
 
 from abraxasOne.loadAndClean import loadAndClean
 
-files = ["ankita.txt", "chris_asymm.txt", "chris_c.txt", "chris_pos2.txt", "igor.txt", "igor2.txt", "ankita_pos2_lrRl.txt"]
-start = np.array([250, 200, 100, 100, 100, 800, 100])
-stop = np.array([1750, 1400, 1500, 1700, 3500, 6400, 2700])
+files = ["igor.txt", "ankita.txt", "chris_asymm.txt", "chris_pos2.txt", "chris_c.txt", "ankita_pos2_lrRl.txt", "igor2.txt"] #
+start = np.array([400, 200, 100, 100, 100, 100, 3500])
+stop = np.array([3400, 1800, 1500, 1700, 1600, 3000, 6000])
 stop = np.ones([len(start)])*np.min(stop-start)+start
 stop = [int(x) for x in stop]
+
+check_data = False
+if check_data:
+    for i, element in enumerate(files):
+        irData, forceData, quatData, linAccData, angVecData = loadAndClean(element, 10, 2, tSample=0.0165, dirPath="")
+        for j in range(len(irData)):
+            plt.plot(irData[j][start[i]:stop[i],1], label=str(j))
+            plt.title(files[i])
+        plt.legend()
+        plt.show()
 
 dataSet = []
 for i, element in enumerate(files):
     irData, forceData, quatData, linAccData, angVecData = loadAndClean(element, 10, 2, tSample=0.0165, dirPath="")
+    dummyData = np.random.rand(np.size(irData[0][start[0]:stop[0],1]))
     for j in range(len(irData)):
-        irData[j] = irData[j][start[i]:stop[i],1]
+        irData[j] = gaussFilter(0, irData[j][start[i]:stop[i],1]) #*0 + np.random.rand(np.size(dummyData))
+    #irData[1] = irData[0]
     dataSet.append(irData)
 
 dcpc = [] # descriptive common principal component
@@ -34,23 +49,47 @@ loading = []
 p = []
 delta = 90
 for i in range(len(dataSet)):
-    X = dataSet[i]
-    corr = pd.DataFrame(X).corr()
-    #sns.heatmap(corr)
-    U, S, UT = np.linalg.svd(corr)
+    X = np.array(dataSet[i]).transpose()
+    corr = np.array(pd.DataFrame(X).corr())
+    #plt.figure()
+    #plt.title(files[i]+" Corr")
+    #sns.heatmap(np.abs(corr))
+    U, variance, UT = np.linalg.svd(corr, full_matrices=True)
+    #variance = np.round(variance, 12)
+    #U = np.round(U, 12).T
+    #plt.figure()
+    #plt.title(files[i]+" U")
+    #sns.heatmap(np.abs(U))
     loading.append(U)
-    variance = S
     percentVar = 100*variance/np.sum(variance)
-    print(percentVar)
-    for i in range(len(percentVar)):
-        if (np.sum(percentVar[0:i])>delta):
-            print(i)
-            print("test")
+    for j in range(len(percentVar)+1):
+        if (np.sum(percentVar[0:j])>=delta):
+            p.append(j)
+            print(j)
             break
-    print("test2")
-    p.append(np.sum(percentVar[percentVar>90]/percentVar[percentVar>90]))
-
+print(p)
+p = np.max(p)
+H = []
+for i in range(len(dataSet)):
+    L = loading[i][:p,::]
+    if i==0:
+        H.append(np.matmul(L.transpose(),L))
+    else:
+        H.append(H[i-1] + np.matmul(L.transpose(),L))
+    #plt.figure()
+    #plt.title(files[i]+" H")
+    #sns.heatmap(np.abs(H[i])/np.max(np.max(H[i])))
+H = H[len(H)-1]
+V, S, VT = np.linalg.svd(H)
+#V = np.round(V, 12)
+DCPC = V[:p,::]
+#plt.figure()
+#plt.title("H")
+#sns.heatmap(np.abs(H[len(dataSet)-1]))
+for i in range(p):
+    print(np.sum(np.abs(DCPC[i,::])*np.abs(DCPC[i,::])))
+plt.figure()
+plt.title("DCPC")
+plt.imshow(np.abs(DCPC))
+plt.colorbar()
 plt.show()
-
-
-    #U, S, UT = np.linalg.svd(, full_matrices=True)
--- test.py
+++ test.py
@@ -1,78 +1,16 @@
 import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
-import peakutils as pu
-from statsmodels import robust
 
-## load data
-from abraxasOne.loadAndClean import loadAndClean
-files = ["ankita.txt", "chris_asymm.txt", "chris_c.txt", "chris_pos2.txt", "igor.txt", "igor2.txt", "ankita_pos2_lrRl.txt"]
-start = np.array([250, 200, 100, 100, 100, 800, 100])
-stop = np.array([1750, 1400, 1500, 1700, 3500, 6400, 2700])
+a = np.array([[2, 4],
+              [1, 3],
+              [0, 0],
+              [0, 0]])
 
-def autoCorr(series, startLagFrom=0, lagUpTo=1):
-    y = []
-    for i in range(lagUpTo):
-        y.append(pd.Series.autocorr(pd.Series(series), lag=i))
-    x = np.linspace(startLagFrom, lagUpTo, (lagUpTo - startLagFrom))
-    return x, np.array(y)[startLagFrom:]
+U, s, VT = np.linalg.svd(a, full_matrices=True)
 
-corrTotal = []
-for k, element in enumerate(files):
-    dir = element
-    irData, forceData, quatData, linAccData, angVecData = loadAndClean(dir, 10, 2, tSample = 0.0165, dirPath = "")
+s = np.diag(s)
+s = np.concatenate([s, np.zeros([2,2])])
 
-    X = []
-    autoCorrX = []
-    corrX = []
-    autoCorrPeakPosOfDataSet = []
-    autoCorrPeakValOfDataSet = []
-    begin = start[k]
-    end = stop[k]
-    for i in range(len(irData)):
-        data = np.abs(irData[i][begin:end,1])
-        autoCorrAxis, autoCorrData = autoCorr(series=data, startLagFrom=25, lagUpTo=200)
-        indices = pu.indexes(autoCorrData, thres=0.1 / max(autoCorrData), min_dist=1)
-        autoCorrPeak = np.array([autoCorrAxis[np.max(autoCorrData[indices])==autoCorrData], np.max(autoCorrData[indices])])
-        autoCorrPeakPosOfDataSet.append(autoCorrPeak[0])
-        autoCorrPeakValOfDataSet.append(autoCorrPeak[1])
-        #plt.scatter(autoCorrPeak[0], autoCorrPeak[1], edgecolors='r')
-        #plt.plot(autoCorrAxis, autoCorrData)
-        #plt.show()
-        autoCorrX.append(autoCorrData)
-        corrXtemp = []
-        for j in range(len(irData)):
-            data2 = irData[j][begin:end,1]
-            corrXtemp.append(pd.Series(data).corr(other=pd.Series(data2)))
-        corrX.append(corrXtemp)
-        #plt.plot(autoCorrData)
-
-    autoCorrPeakPosOfDataSet = np.array(autoCorrPeakPosOfDataSet)
-    autoCorrPeakValOfDataSet = np.array(autoCorrPeakValOfDataSet)
-    #plt.hist(autoCorrPeakPosOfDataSet,color='b')
-    #plt.hist(autoCorrPeakPosOfDataSet[np.argmax(autoCorrPeakValOfDataSet)],color='r')
-    autoCorrPeakPosOfDataSet = int(np.mean(autoCorrPeakPosOfDataSet))
-    for i in range(len(irData)):
-        data = irData[i][begin:end,1]
-        data = data[:int(len(data)-(len(data)%autoCorrPeakPosOfDataSet))]
-        numberOfWindows = int(len(data) / autoCorrPeakPosOfDataSet)
-        meanOfSensor = np.reshape(data, [numberOfWindows, autoCorrPeakPosOfDataSet])
-        for j in range(numberOfWindows):
-            plt.plot(data[:autoCorrPeakPosOfDataSet])
-            plt.plot(meanOfSensor[j,::])
-
-        meanOfSensor = np.mean(meanOfSensor, 0)
-        plt.show()
-        plt.figure()
-        plt.plot(meanOfSensor)
-        #plt.plot(meanOfSensor)
-        plt.show()
-    plt.show()
-    corrX = np.array(corrX)
-    #plt.title(dir)
-    corrTotal.append(corrX)
-    plt.show()
-
-corrTotal = np.array(corrTotal)
-corrTotal = np.sum(corrTotal,0)/len(corrTotal)
-
+print("U:\n {}".format(U))
+print("s:\n {}".format(s))
+print("VT:\n {}".format(VT.T))
+print(U.dot(s).dot(VT.T))
